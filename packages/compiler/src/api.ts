2ba7a7e3f07e9d4a6fc36686d6367dd1889dc25f
James Ferguson <jamesferguson497@gmail.com>
2020-02-23 09:32:12 +1100

    return error messages from compile function

--- packages/compiler/src/api.ts
+++ packages/compiler/src/api.ts
@@ -4,24 +4,37 @@ import { evaluateExpression } from './type-checker/evaluate';
 import { runTypePhase } from './type-checker/run-type-phase';
 import { stripNode } from './type-checker/strip-nodes';
 import { TypedNode } from './type-checker/type-check';
+import { Expression } from './type-checker/types/expression';
+import { Message } from './type-checker/types/message';
 import { Value } from './type-checker/types/value';
 
-export function compile(code: string): TypedNode | undefined {
-  const { messages, value: expression } = parse(code);
-  if (expression) {
-    const [typeMessages, typedNode] = runTypePhase(expression);
+export { TypedNode } from './type-checker/type-check';
 
-    if (typeMessages.length === 0) {
-      return typedNode;
-    }
+export interface CompileResult {
+  expression?: Expression;
+  node?: TypedNode;
+  messages: Message[];
+}
+
+export function compile(code: string): CompileResult {
+  const { value: expression } = parse(code);
+  if (!expression) {
+    return { messages: ['Failed to parse code'] };
   }
-  return undefined;
+
+  const [typeMessages, typedNode] = runTypePhase(expression);
+
+  return {
+    expression: stripNode(typedNode),
+    node: typedNode,
+    messages: typeMessages,
+  };
 }
 
 export function evaluate(code: string): Value | undefined {
-  const typedNode = compile(code);
-  if (typedNode) {
-    return evaluateExpression(evaluationScope())(stripNode(typedNode));
+  const { expression } = compile(code);
+  if (expression) {
+    return evaluateExpression(evaluationScope())(expression);
   }
   return undefined;
 }
