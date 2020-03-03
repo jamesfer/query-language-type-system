8732a4f0f2d41457443a77c2cc08eda2e584bf35
James Ferguson <jamesferguson497@gmail.com>
2020-03-03 23:27:46 +1100

    use webpack for bundling editor

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,7 +1,21 @@
-// import * as Buffer from 'buffer';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+import '../index.css';
 import App from './app';
 
-// (window as any).Buffer = Buffer;
+// Insert html content into the body
+const element = document.createElement('div');
+element.innerHTML = `
+  <div class="wrapper">
+    <div class="editor">
+      <textarea class="editor-input" id="editor" style="width: 500px; height: 500px;"></textarea>
+    </div>
+    <div class="editor">
+      <textarea class="editor-input" id="output"></textarea>
+    </div>
+  </div>
+`;
+document.body.appendChild(element);
 
 const editorElement = document.getElementById('editor');
 const outputElement = document.getElementById('output');
