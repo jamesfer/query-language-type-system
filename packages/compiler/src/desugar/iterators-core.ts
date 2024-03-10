7bd083fe5a9eae665fac1621d05b2965f30b61ca
James Ferguson <jamesferguson497@gmail.com>
2024-03-10 16:52:00 +0100

    Fix type-checker tests

--- packages/compiler/src/desugar/iterators-core.ts
+++ packages/compiler/src/desugar/iterators-core.ts
@@ -1,12 +1,19 @@
 import { Kind, URIS } from 'fp-ts/lib/HKT';
 
-export type MapIterator<E extends URIS, A, B> = (f: (a: A) => B) => (expression: Kind<E, A>) => Kind<E, B>
-export type ReductionIterator<E extends URIS, A, B> = (f: (a: A) => B) => (expression: Kind<E, A>) => B;
+export type MapIterator<E extends URIS, A, B> =
+  (f: (a: A) => B) => (expression: Kind<E, A>) => Kind<E, B>;
+export type ReductionIterator<E extends URIS, A, B> =
+  (f: (a: A) => B) => (expression: Kind<E, A>) => B;
 
 export type ReductionIteratorMap<Keys extends string, E extends { kind: URIS }, A, B> = (
   // E extends { kind: infer K }
   //   ? K extends string
-  { [k in Keys]: k extends URIS ? ((expression: E extends { kind: k } ? Kind<k, A> : never) => B) : never }
+  {
+    [k in Keys]: k extends URIS
+      ? ((expression: E extends { kind: k }
+        ? Kind<k, A> : never) => B)
+    : never
+  }
     // : never
     // : never
 );
@@ -27,25 +34,31 @@ export type IteratorMapIterator<K extends URIS, A, B> = ((f: (a: A) => B) => (ex
 export type IteratorMap<Keys extends string, E extends { kind: URIS }, A, B> = (
   // E extends { kind: infer K }
   //   ? K extends string
-  { [k in Keys]: k extends URIS ? E extends { kind: k } ? IteratorMapIterator<k, A, B> : never : never }
+  {
+    [k in Keys]: k extends URIS
+      ? E extends { kind: k }
+        ? IteratorMapIterator<k, A, B>
+      : never
+    : never
+  }
   // : never
   // : never
 );
 
 export function combineIteratorMap<EU extends URIS, E extends { kind: URIS } & Kind<EU, any>, A, B>(
-  iterators: IteratorMap<E['kind'], E, A, B>
+  iterators: IteratorMap<E['kind'], E, A, B>,
 ): (f: (a: A) => B) => (input: Kind<EU, A> & { kind: URIS }) => Kind<EU, B> {
   return f => (input) => {
     if (input.kind in iterators) {
-      return iterators[input.kind](f)(input);
+      return iterators[input.kind as E['kind']](f)(input);
     }
-    throw new Error(`Unknown iterator for object with a kind of ${input.kind}. Known iterators: ${Object.keys(iterators).join(', ')}`);
-  }
+    throw new Error(`Unknown iterator for object with a kind of ${(input.kind)}. Known iterators: ${Object.keys(iterators).join(', ')}`);
+  };
 }
 
 export type Prop<K extends string, V> = {
   [k in K]: V;
-}
+};
 
 export function passThroughIterator<K extends string>(
   key: K,
